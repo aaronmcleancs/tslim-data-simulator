39c1e1bedf72c6bf73f950c2a748098c6dcc4f1e
Aaron McLean <aaronmclean26@gmail.com>
2025-03-20 23:14:10 -0400

    just headers

--- cpp/Battery.cpp
+++ cpp/Battery.cpp
@@ -1,22 +1,2 @@
 #include "Battery.h"
 
-// Example skeleton for initial repo
-// Feel free to change any of this to your implementation
-
-Battery::Battery(QObject *parent)
-    : QObject(parent), chargeLevel(100)
-{
-}
-
-int Battery::getChargeLevel() const
-{
-    return chargeLevel;
-}
-
-void Battery::setChargeLevel(int level)
-{
-    if(chargeLevel != level){
-        chargeLevel = level;
-        emit chargeLevelChanged(chargeLevel);
-    }
-}
