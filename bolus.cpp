831a29097d0b2edb22e1276e5658bd9cb0d849c9
Shreyansh145 <shreyanshparimalpate@cmail.carleton.ca>
2025-04-03 19:19:16 -0400

    Bugs solved for Bolus

--- bolus.cpp
+++ bolus.cpp
@@ -8,12 +8,7 @@ bolus::bolus(QWidget *parent) :
     ui(new Ui::bolus)
 {
     ui->setupUi(this);
-    ui->groupBox->setVisible(false);
-        ui->groupBox_2->setVisible(false);
-        ui->radioButton->setVisible(false);
-        ui->radioButton_2->setVisible(false);
-        ui->groupBox_3->setVisible(false);
-        ui->label_8->setVisible(false);
+
 }
 
 bolus::~bolus()
@@ -21,61 +16,4 @@ bolus::~bolus()
     delete ui;
 }
 
-void bolus::on_pushButton_clicked()
-{
-    ui->pushButton_2->setEnabled(true);
-       ui->pushButton_3->setEnabled(true);
-       ui->groupBox->setVisible(false);
-       ui->groupBox_2->setVisible(false);
-       ui->radioButton->setVisible(false);
-       ui->radioButton_2->setVisible(false);
-    emit mainShift();
-}
-
-
-void bolus::on_pushButton_2_clicked()
-{
-    QPushButton *pb2 = findChild<QPushButton*>("pushButton_3");
-       pb2->setEnabled(false);
-       ui->groupBox->setVisible(true);
-}
-
-
-void bolus::on_pushButton_3_clicked()
-{
-    ui->pushButton_2->setDisabled(true);
-       ui->groupBox_2->setVisible(true);
-       CGM c1;
-       double carbs = c1.getCurrentCarbs();
-       double glucose = c1.getCurrentGlucoseLevel();
-       ui->label_6->setText(QString::number(carbs));
-       ui->label_7->setText(QString::number(glucose));
-       qDebug()<<"Automatic mode..... ";
-
-}
-
-
-void bolus::on_pushButton_4_clicked()
-{
-    BolusCalculator b1;
-        int bolus = b1.total_bolus(ui->lineEdit->text().toInt(), ui->lineEdit_2->text().toInt());
-        qDebug()<<"bolus"<<bolus;
-        ui->radioButton->setVisible(true);
-        ui->label_8->setVisible(true);
-        ui->radioButton_2->setVisible(true);
-}
-
-
-void bolus::on_pushButton_5_clicked()
-{
-    ui->radioButton->setVisible(true);
-    ui->radioButton_2->setVisible(true);
-    ui->label_8->setVisible(true);
-}
-
-
-void bolus::on_radioButton_clicked()
-{
-    ui->groupBox_3->setVisible(true);
-}
 
--- bolus.h
+++ bolus.h
@@ -19,17 +19,7 @@ signals:
     void mainShift();
 
 private slots:
-    void on_pushButton_clicked();
 
-    void on_pushButton_2_clicked();
-
-    void on_pushButton_3_clicked();
-
-    void on_pushButton_4_clicked();
-
-    void on_pushButton_5_clicked();
-
-    void on_radioButton_clicked();
 
 private:
     Ui::bolus *ui;
